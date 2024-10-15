328f162179e28475bfc5f2623d9d1e3ac91c8422
rodolphonikqg <rodolpho.oliveira@acct.global>
2024-10-15 17:57:57 -0300

    test: add unit tests for postController.ts getPost

--- src/controllers/postController.test.ts
+++ src/controllers/postController.test.ts
@@ -43,4 +43,63 @@ describe('postController', () => {
       expect(res.json).toHaveBeenCalledWith(mockPosts);
     });
   });
+
+  describe('getPost', () => {
+    it('Should fetch a post by id and return it as JSON', async () => {
+      const req = {
+        params: { id: 'PJw207NixHaNZI8F' },
+      } as unknown as Request;
+      const res = {
+        json: jest.fn(),
+      } as unknown as Response;
+
+      const mockPost = mockPosts[0];
+      (postService.getPostById as jest.Mock).mockResolvedValue(mockPost);
+
+      await postController.getPost(req, res);
+
+      expect(postService.getPostById).toHaveBeenCalledWith('PJw207NixHaNZI8F');
+      expect(res.json).toHaveBeenCalledWith(mockPost);
+    });
+
+    it('Should return 404 if post is not found', async () => {
+      const req = {
+        params: { id: 'any_id' },
+      } as unknown as Request;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response;
+
+      const error = new Error('Post not found');
+      (postService.getPostById as jest.Mock).mockRejectedValue(error);
+
+      await postController.getPost(req, res);
+
+      expect(postService.getPostById).toHaveBeenCalledWith('any_id');
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({ message: 'Post not found' });
+    });
+
+    it('Should return 500 for other errors', async () => {
+      const req = {
+        params: { id: 'PJw207NixHaNZI8F' },
+      } as unknown as Request;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      } as unknown as Response;
+
+      const error = new Error('Internal server error');
+      (postService.getPostById as jest.Mock).mockRejectedValue(error);
+
+      await postController.getPost(req, res);
+
+      expect(postService.getPostById).toHaveBeenCalledWith('PJw207NixHaNZI8F');
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Internal server error',
+      });
+    });
+  });
 });
